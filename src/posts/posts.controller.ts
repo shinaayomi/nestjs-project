49385adb1e4e635ae872d256b56f9a34dc9a374b
shinaayomi <lateef718@gmail.com>
2025-07-13 14:09:53 +0100

    Post crud

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,60 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { PostsService } from './posts.service';
+import { Post as PostInterface } from './interface/post.interface';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  @Get()
+  findAll(@Query('search') search?: string): PostInterface[] {
+    const extractAllPosts = this.postsService.findAll();
+
+    if (search) {
+      return extractAllPosts.filter((singlePost) =>
+        singlePost.title.toLowerCase().includes(search.toLowerCase()),
+      );
+    }
+
+    return extractAllPosts;
+  }
+
+  @Get(':id') // dynamic id
+  findOne(@Param('id', ParseIntPipe) id: number): PostInterface {
+    return this.postsService.findOne(id);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(
+    @Body() createPostData: Omit<PostInterface, 'id' | 'createdAt'>,
+  ): PostInterface {
+    return this.postsService.create(createPostData);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updatePostData: Partial<Omit<PostInterface, 'id' | 'reatedAt'>>,
+  ): PostInterface {
+    return this.postsService.update(id, updatePostData);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(@Param('id', ParseIntPipe) id: number): void {
+    this.postsService.remove(id);
+  }
+}
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,78 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Post } from './interface/post.interface';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  private posts: Post[] = [
+    {
+      id: 1,
+      title: 'First',
+      content: 'First post content',
+      authorName: 'Shinaayomi',
+      createdAt: new Date(),
+    },
+  ];
+
+  findAll(): Post[] {
+    return this.posts;
+  }
+
+  findOne(id: number): Post {
+    const singlePost = this.posts.find((post) => post.id === id);
+
+    if (!singlePost) {
+      throw new NotFoundException(`Post with ID ${id} is not found`);
+    }
+
+    return singlePost;
+  }
+
+  create(createPostData: Omit<Post, 'id' | 'createdAt'>): Post {
+    const newPost: Post = {
+      id: this.getNextId(),
+      ...createPostData,
+      createdAt: new Date(),
+    };
+
+    this.posts.push(newPost);
+    return newPost;
+  }
+
+  update(id: number, updatePostData: Partial<Omit<Post, 'id' | 'createdAt'>>) {
+    const currentPostIndexToEdit = this.posts.findIndex(
+      (post) => post.id === id,
+    );
+
+    // Did not exist/present
+    if (currentPostIndexToEdit === -1) {
+      throw new NotFoundException(`Post with ID ${id} is not found`);
+    }
+
+    this.posts[currentPostIndexToEdit] = {
+      ...this.posts[currentPostIndexToEdit],
+      ...updatePostData,
+      updatedAt: new Date(),
+    };
+    return this.posts[currentPostIndexToEdit];
+  }
+
+  remove(id: number): { message: string } {
+    const currentPostIndexToDelete = this.posts.findIndex(
+      (post) => post.id === id,
+    );
+
+    if (currentPostIndexToDelete === -1) {
+      throw new NotFoundException(`Post with ID ${id} is not found`);
+    }
+
+    this.posts.splice(currentPostIndexToDelete, 1);
+
+    return { message: `Post with ID ${id} has been deleted` };
+  }
+
+  private getNextId(): number {
+    return this.posts.length > 0
+      ? Math.max(...this.posts.map((post) => post.id)) + 1
+      : 1;
+  }
+}
